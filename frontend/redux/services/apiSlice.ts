187a98bf4363e96acae30f4d13b5aef610ef485e
Programming Bees <hasanakash799@gmail.com>
2023-08-03 01:06:49 +0600

    chore: creating apiSlice

---
+++ frontend/redux/services/apiSlice.ts
@@ -0,0 +1,51 @@
+import type {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/query";
+import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { Mutex } from "async-mutex";
+
+// create a new mutex
+const mutex = new Mutex();
+const baseQuery = fetchBaseQuery({ 
+    baseUrl: `${process.env.NEXT_PUBLIC_HOST}/api`,
+    credentials: 'include',
+ });
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  // wait until the mutex is available without locking it
+  await mutex.waitForUnlock();
+  let result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+    // checking whether the mutex is locked
+    if (!mutex.isLocked()) {
+      const release = await mutex.acquire();
+      try {
+        const refreshResult = await baseQuery(
+          "/refreshToken",
+          api,
+          extraOptions
+        );
+        if (refreshResult.data) {
+          api.dispatch(tokenReceived(refreshResult.data));
+          // retry the initial query
+          result = await baseQuery(args, api, extraOptions);
+        } else {
+          api.dispatch(loggedOut());
+        }
+      } finally {
+        // release must be called once the mutex should be released again.
+        release();
+      }
+    } else {
+      // wait until the mutex is available without locking it
+      await mutex.waitForUnlock();
+      result = await baseQuery(args, api, extraOptions);
+    }
+  }
+  return result;
+};
